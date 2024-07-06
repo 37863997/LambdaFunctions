0a6fd96a77cd44379d92ef2db19aaf536b6b6439
Jesse Mugo <145917688+37863997@users.noreply.github.com>
2024-07-06 12:47:14 -0700

    Update lambda.py

---
+++ lambda.py
@@ -0,0 +1,38 @@
+Math_Operations=lambda a,b:a+b
+"""
+ add two numbers and return the results
+
+    This function takes two numerical inputs and returns the results
+    it demonstrates a simple addition operation
+
+    parameters:
+    a(int).The first number to add.
+    b(int).The second number to add.
+    
+    Return:
+    int.The sum of the two numbers.
+"""
+Product_Operation=lambda a,b:a*b
+"""
+  product of two numbers and return the results
+
+    This function takes two numerical inputs and returns the results
+    it demonstrates a simple multiplication  operation
+
+    parameters:
+    a(int).The first number to add.
+    b(int).The second number to add.
+    
+    Return:
+    (int ).The product of the two numbers.
+"""
+print(Math_Operations(3,5))
+print(Product_Operation(3,5))
+
+
+
+
+  
+
+
+ 
